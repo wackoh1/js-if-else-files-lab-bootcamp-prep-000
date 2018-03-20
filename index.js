e6a5a8f40b2cfc44d541719aba334faf180b02f1
Jack O'Nolan <onolan.j@gmail.com>
2018-03-20 14:22:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var height = "74"
 
 if ( name === "Susan" ) {
   console.log("Name is correct")
+} else {"Expected the name to be Susan"
 }
+
 
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
